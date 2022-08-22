58c3f94c4d169a497f9128375fa1209180e06b6c
Ortolani077 <ortolani077@gmail.com>
2022-08-22 15:28:31 -0300

    aluno

--- primeiro-programa-eclipse/src/CusroJava/classes/Aluno.java
+++ primeiro-programa-eclipse/src/CusroJava/classes/Aluno.java
@@ -13,41 +13,21 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	private Disciplina disciplina =new Disciplina();
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
 	
 	
-	public Aluno() {
-		
-		
-			
-		}
-	public double getNota1() {
-		return nota1;
-	}
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-	public double getNota2() {
-		return nota2;
-	}
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-	public double getNota3() {
-		return nota3;
-	}
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-	public double getNota4() {
-		return nota4;
-	}
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
+	
+	
+	
+	
+	
+	
 	public Aluno (String nomePadrao) {
 		
 		nome=nomePadrao;
@@ -135,7 +115,9 @@ public void setSerieMatriculado(String serieMatriculado) {
 
 /* metodo retorna media do aluno*/
 public double getMediaNota() {
-	return (nota1+ nota2+nota3+nota4)/4;
+	return (disciplina.getNota1()+ disciplina.getNota2()+disciplina.getNota3()
+	
+	+disciplina.getNota4())/4;
 	
 }
 
@@ -158,8 +140,49 @@ public String getAlunoAprovado2() {
 		return "Aluno está reporvado !!!!";
 	}
 	
+	
+	
+	
 }
+
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+	result = prime * result + ((numeroCpf == null) ? 0 : numeroCpf.hashCode());
+	return result;
 }
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Aluno other = (Aluno) obj;
+	if (nome == null) {
+		if (other.nome != null)
+			return false;
+	} else if (!nome.equals(other.nome))
+		return false;
+	if (numeroCpf == null) {
+		if (other.numeroCpf != null)
+			return false;
+	} else if (!numeroCpf.equals(other.numeroCpf))
+		return false;
+	return true;
+}
+
+
+
+}
+
+
+
+
 
 
 
