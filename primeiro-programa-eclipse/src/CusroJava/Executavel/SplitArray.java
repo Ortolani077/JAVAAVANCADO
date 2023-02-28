2439b58ff5c429b38dea9464f28c0553b7544e7d
rodrigo ortolani <ortolani077@gmail.com>
2023-02-27 21:58:03 -0300

    Próxima AULA - Tempo de interrupção com Sleep

---
+++ primeiro-programa-eclipse/src/CusroJava/Executavel/SplitArray.java
@@ -0,0 +1,55 @@
+package CusroJava.Executavel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SplitArray {
+
+	public static void main(String[] args) {
+		
+		String texto = "Rodrigo,Java, 80, 70, 90, 89";
+		String[] valoresArray=texto.split(",");
+		
+		/*System.out.println("Nome: "   +valoresArray[0]);
+		System.out.println("Curso: "  +valoresArray[1]);
+		System.out.println("Nota1: "  +valoresArray[2]);
+		System.out.println("Nota2: "  +valoresArray[3]);
+		System.out.println("Nota3: "  +valoresArray[4]);
+		System.out.println("Nota4: "  +valoresArray[5]);
+		
+	
+	              /*Convertendo um array em uma lista*/
+	
+	List<String> list=Arrays.asList(valoresArray);
+	
+	
+	for (String valorString : list) {
+		//System.out.println(valorString);
+	
+	
+	
+	
+	
+	
+	
+	}
+		
+		
+		/*Converter uma lista para array*/
+		
+	String[] conversaoArray = list.toArray(new String[6]);
+	
+	
+	for(int pos=0; pos< conversaoArray.length; pos++) {
+		System.out.println(conversaoArray[pos]);
+		
+		
+		
+	}
+	}
+	
+	
+	
+	
+	
+}
